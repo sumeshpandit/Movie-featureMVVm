c2ca6c7d46d1ab4c80df83ff3432216540442859
sumeshpandit <sumeshkpandit@gmail.com>
2021-05-14 16:42:20 +0530

    Separated into packages

--- app/src/main/java/com/sumeshpandit/movie_featuremvvm/Api/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.sumeshpandit.movie_featuremvvm
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("/3/movie/popular?api_key=190468dc1c1669233fd28bec0dacca7d")
-    fun getMovieList(): Call<MovieResponse>
-}
--- app/src/main/java/com/sumeshpandit/movie_featuremvvm/Model/MovieResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.sumeshpandit.movie_featuremvvm
-
-import com.google.gson.annotations.SerializedName
-import com.sumeshpandit.movie_featuremvvm.Model.MovieData
-
-class MovieResponse (
-    @SerializedName("results")
-    var movies : List<MovieData> = mutableListOf()
-)
