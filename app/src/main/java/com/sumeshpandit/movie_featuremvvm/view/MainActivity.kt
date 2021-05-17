2908f20c9d687e25d473845c1fd4c5d73b4fb058
sumeshpandit <sumeshkpandit@gmail.com>
2021-05-17 20:38:44 +0530

    edited code

--- app/src/main/java/com/sumeshpandit/movie_featuremvvm/view/MainActivity.kt
+++ app/src/main/java/com/sumeshpandit/movie_featuremvvm/view/MainActivity.kt
@@ -5,21 +5,20 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sumeshpandit.movie_featuremvvm.R
 import com.sumeshpandit.movie_featuremvvm.databinding.ActivityMainBinding
 import com.sumeshpandit.movie_featuremvvm.viewModel.MovieViewModel
-import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var myViewModel:MovieViewModel
+    private val myViewModel:MovieViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        myViewModel=getViewModel()
+
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         getMostPopularMovie(binding)
     }
